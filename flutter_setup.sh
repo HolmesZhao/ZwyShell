7e401699c3dae5cfb539eaede84672d0a347958a
HolmesZhao <>
2024-05-14 23:02:17 +0800

    feat: flutter 安装脚本

---
+++ flutter_setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash --login
+clear
+
+if [ ! -d "fluttersdk/flutter/bin" ];then
+    echo ""
+    echo "还没有下载 fluttersdk"
+    curl -o fluttersdk.zip http://drive.zwyxxd.press:5170/zwy/fluttersdk/flutter_macos_arm64_3.10.4-stable.zip
+    if [ ! -f "fluttersdk.zip" ];then
+        echo ""
+        echo "下载 fluttersdk.zip 失败"
+        exit 0
+    else
+        echo ""
+        echo "下载 fluttersdk.zip 成功"
+        
+        unzip -d ./fluttersdk ./fluttersdk.zip
+        
+        if [ ! -d "fluttersdk" ];then
+            echo ""
+            echo "解压 fluttersdk.zip 失败"
+        else
+            echo ""
+            echo "解压 fluttersdk.zip 成功"
+            
+            rm -f ./fluttersdk.zip
+            
+            echo ""
+            echo "下面的环境变量需要写入 ~/.bashrc 或者 ./zshrc"
+            echo "export PATH=\$PATH:$PWD/fluttersdk/flutter/bin"
+            
+            flutter --version
+        fi
+    fi
+else
+    echo ""
+    echo "已经下载 fluttersdk:"
+    echo ""
+    flutter --version
+fi
+
+
